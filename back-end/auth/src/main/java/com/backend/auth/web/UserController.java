1f0074fba50a3a0ac0c47c10273716d576fe635b
Aisha Abdul Quadir <aisha.abdulquadirzaifullah@gmail.com>
2025-12-07 21:33:37 +0300

    Update UserController.java

--- back-end/auth/src/main/java/com/backend/auth/web/UserController.java
+++ back-end/auth/src/main/java/com/backend/auth/web/UserController.java
@@ -2,7 +2,9 @@ package com.backend.auth.web;
 
 import java.util.Collection;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,7 @@ import com.backend.auth.web.dto.UserDTO;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class UserController {
 	
