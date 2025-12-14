be222c2be85410eac2a1ffcd1a305150875bf676
Aisha Abdul Quadir <aisha.abdulquadirzaifullah@gmail.com>
2025-12-15 00:55:33 +0300

    generate hash functions done

--- back-end/auth/src/main/java/utilities/Hashing.java
+++ back-end/auth/src/main/java/utilities/Hashing.java
@@ -1,5 +1,6 @@
 package utilities;
 
+import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 import java.util.Base64;
 
@@ -20,6 +21,8 @@ public class Hashing {
 	
 	
 	private static final int saltByteSize = 16;
+	
+	private static final int HASH_LENGTH = 32;
 	
 	private static byte[] generateSalt() {
 		
@@ -45,16 +48,49 @@ public class Hashing {
 		
 		Argon2BytesGenerator generator = new Argon2BytesGenerator();
 		generator.init(params);
-		
-		System.out.println(generator.toString());
-		System.out.println(salt.toString());
-		
-		String encodedSalt = Base64.getEncoder().encodeToString(salt);
-		System.out.println(encodedSalt);
-		
-		return null;
+		byte[] hash = new byte[HASH_LENGTH];
+	    generator.generateBytes(password.getBytes(StandardCharsets.UTF_8), hash, 0, hash.length);
+			
+		return formatHashToString(hash,salt);
 		
 		
+	}
+	
+	private static String formatHashToString(byte[] hash, byte[] salt) {
+		
+		String hashBase64 = Base64.getEncoder().encodeToString(hash);
+		String saltBase64 = Base64.getEncoder().encodeToString(salt);
+		
+		StringBuilder hashString = new StringBuilder().append("$argon2");
+		
+		System.out.println(hashBase64);
+		System.out.println(saltBase64);
+		
+		switch (TYPE) {
+			case Argon2Parameters.ARGON2_d: 
+				hashString.append("d");
+				break;
+			case Argon2Parameters.ARGON2_i:
+				hashString.append("i");
+				break;
+			case Argon2Parameters.ARGON2_id:
+				hashString.append("id");
+				break;
+		}
+		
+		hashString.append(
+				String.format(
+						"$v=%d$m=%d,t=%d,p=%d$%s$%s", 
+						VERSION,
+						MEMORY_SIZE,
+						ITERATIONS,
+						PARALLELISM,
+						saltBase64,
+						hashBase64
+				)
+		);
+		
+		return hashString.toString();	
 		
 	}
 	
