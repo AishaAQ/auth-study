7fb0d8314a6dba030155e3884fdc7c439cdbd89a
Aisha Abdul Quadir <aisha.abdulquadirzaifullah@gmail.com>
2026-01-02 20:08:24 +0300

    created user table

--- back-end/auth/src/main/java/com/backend/auth/model/User.java
+++ back-end/auth/src/main/java/com/backend/auth/model/User.java
@@ -1,6 +1,15 @@
 package com.backend.auth.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "auth_user")
 public class User {
+	
+	@Id
+	private String userId;
 	
 	private String email;
 	
